f40e6b99d0f139ab0b72f9c306579b35ed3525ef
muhsinking <muhsinking@gmail.com>
2015-11-05 15:04:39 -0500

    Remove instance declaration from ByteUtils test

--- src/test/groovy/ByteUtilsTest.groovy
+++ src/test/groovy/ByteUtilsTest.groovy
@@ -1,20 +1,21 @@
 /**
- * Created by muhsinking on 11/4/15.
+ * Created by muhsinking on 11/5/15.
  */
+
 class ByteUtilsTest extends GroovyTestCase {
-    def bu = new ByteUtils()
+//    def bu = new ByteUtils()
     def longArray = [0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] as byte[]
     def x = 3
 
     void testLongToBytes() {
-        def result = bu.longToBytes(x)
+        def result = ByteUtils.longToBytes(x)
         assert result == longArray
 
     }
 
     void testBytesToLong() {
 
-        def result = bu.bytesToLong(longArray)
+        def result = ByteUtils.bytesToLong(longArray)
         assert result == x
     }
 }
