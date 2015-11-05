22a265a56c64076c26211e1c2d3857fbbc78e200
muhsinking <muhsinking@gmail.com>
2015-11-05 18:56:44 -0500

    Switch time measurement to System.nanoTime()

--- src/main/java/UDPClient.java
+++ src/main/java/UDPClient.java
@@ -15,7 +15,7 @@ class UDPClient
         DatagramSocket clientSocket = new DatagramSocket();
         InetAddress IPAddress = InetAddress.getByName("localhost");
         byte[] sendData = new byte[2048];
-        byte[] receiveData = new byte[2048];
+        byte[] receiveData = new byte[20480];
 
         String sentence = inFromUser.readLine();
 
@@ -23,7 +23,9 @@ class UDPClient
 
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 
-        long startTime = date.getTime();
+        long startTime = System.nanoTime();
+
+//        Thread.sleep(1000);
 
         byte [] sendTime = ByteUtils.longToBytes(startTime);
 
@@ -33,9 +35,9 @@ class UDPClient
 
         clientSocket.receive(receivePacket);
 
-        long receiveTime = date.getTime();
+        long receiveTime = System.nanoTime();
 
-        long elapsedTime = receiveTime-startTime;
+        long elapsedTime = (receiveTime-startTime)/1000000; // convert to milliseconds
 
         String modifiedSentence = new String(receivePacket.getData());
 
