9518a615039f4feb55a38daeb41b0805c45378f2
muhsinking <muhsinking@gmail.com>
2015-12-04 14:30:15 -0500

    Modularize tcp client/server

--- src/main/java/TCPClient.java
+++ src/main/java/TCPClient.java
@@ -4,20 +4,43 @@
 import java.io.*;
 import java.net.*;
 
-class TCPClient
+public class TCPClient
 {
+    Socket socket;
+
+    TCPClient(String address, int sock) throws IOException {
+        this.socket = new Socket(address, sock);
+    }
+
     public static void main(String argv[]) throws Exception
     {
-        String sentence;
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6789);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + '\n');
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
-        clientSocket.close();
+//        String sentence;
+//        String modifiedSentence;
+//        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+//        Socket clientSocket = new Socket("localhost", 6789);
+//        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+//        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//        sentence = inFromUser.readLine();
+//        outToServer.writeBytes(sentence + '\n');
+//        modifiedSentence = inFromServer.readLine();
+//        System.out.println("FROM SERVER: " + modifiedSentence);
+//        clientSocket.close();
+
+        TCPClient client = new TCPClient("localhost",6789);
+        client.send(20);
+        int rx = client.receive();
+        System.out.println(rx);
+
     }
+
+    public void send(int n)throws IOException {
+        DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+        outToServer.writeInt(n);
+    }
+
+    public int receive() throws IOException {
+        DataInputStream inFromServer = new DataInputStream(socket.getInputStream());
+        return inFromServer.readInt();
+    }
+
 }
--- src/main/java/TCPServer.java
+++ src/main/java/TCPServer.java
@@ -6,22 +6,52 @@ import java.net.*;
 
 public class TCPServer
 {
+
+    ServerSocket socket;
+
+    TCPServer(int sock) throws IOException {
+        socket = new ServerSocket(sock);
+    }
+
     public static void main(String argv[]) throws Exception
     {
-        String clientSentence;
-        String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(6789);
+        TCPServer server = new TCPServer(6789);
 
-        while(true)
-        {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+        while(true){
+            Socket connection = server.socket.accept();
+            int rx = server.receive(connection);
+            server.send(connection,rx+1);
         }
+
+//        String clientSentence;
+//        String capitalizedSentence;
+//        ServerSocket welcomeSocket = new ServerSocket(6789);
+//
+//        while(true)
+//        {
+//            Socket connectionSocket = welcomeSocket.accept();
+//            BufferedReader inFromClient =
+//                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+//            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+//            clientSentence = inFromClient.readLine();
+//            System.out.println("Received: " + clientSentence);
+//            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+//            outToClient.writeBytes(capitalizedSentence);
+//        }
     }
+
+    public void send(Socket connection, int n)throws IOException {
+        DataOutputStream outToClient = new DataOutputStream(connection.getOutputStream());
+        outToClient.writeInt(n);
+    }
+
+    public int receive(Socket connection) throws IOException {
+        DataInputStream inFromClient = new DataInputStream(connection.getInputStream());
+        return inFromClient.read();
+    }
+
+//    public int receive() throws IOException {
+//        DataInputStream inFromServer = new DataInputStream(socket.getInputStream()));
+//        return inFromServer.readInt();
+//    }
 }
