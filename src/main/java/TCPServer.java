56478aae8d5b1c2afbad5028c48040b02de4c0ce
Mo King <muhsinking@gmail.com>
2015-12-04 11:42:29 -0500

    Create TCP Client/Server

--- src/main/java/TCPServer.java
+++ src/main/java/TCPServer.java
@@ -1,5 +1,27 @@
 /**
  * Created by Muhsin King on 11/14/2015.
  */
-public class TCPServer {
+import java.io.*;
+import java.net.*;
+
+public class TCPServer
+{
+    public static void main(String argv[]) throws Exception
+    {
+        String clientSentence;
+        String capitalizedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while(true)
+        {
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient =
+                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
 }
