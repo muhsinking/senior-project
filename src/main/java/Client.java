069e377e86dc8fb040547bcbcdde3a6cdb9b0551
muhsinking <muhsinking@gmail.com>
2016-02-15 11:23:45 -0500

    Remove deprecated code from client/server

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -92,36 +92,4 @@ public class Client {
         }
         return div;
     }
-
-    // intra-probe gap between a packet pair in microseconds, averaged over n executions
-/*    public long avgPairIPG(int n, int sizeH, int sizeT, InetAddress IP, int port, String resolution) throws IOException {
-        System.out.println("Intra-probe gap between head packet of " +
-                sizeH + " bytes and tail packet of " +
-                sizeT + " bytes, averaged over " + n + " runs:");
-
-        sendInt(n, IP, port);  // send a single int, indicating the number of packet pairs the server should expect to receive
-        sendInt(sizeH, IP, port); // send a single int, indicating the size of the head packets the server should expect
-        sendInt(sizeT, IP, port); // send a single int, indicating the size of the tail packets the server should expect
-
-        DatagramPacket confirmation = receive(4);  // waits to receive confirmation from the server
-
-        long sum = 0;
-
-        for(int i = 0; i < n; i++){sum += packetPairIPG(sizeH, sizeT, IP, port);}
-
-        int div = getResolutionDivider(resolution);
-
-        if(div < 0){
-            div = 1;
-            resolution = "nano";
-        }
-
-        long clientIPG = sum/n/div;
-        DatagramPacket serverResponse = receive(4);
-        int serverIPG = ByteUtils.bytesToInt(serverResponse.getData())/div;
-        System.out.println("Server - " + serverIPG + " " + resolution + "seconds");
-        System.out.println("Client - " + clientIPG + " " + resolution + "seconds");
-        return clientIPG;
-    }*/
-
 }
