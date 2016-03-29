5026c6f78e2a2d3393e7bc36501b23dd1727c1d8
muhsinking <muhsinking@gmail.com>
2016-03-29 13:20:51 -0400

    Add simplified client/server
    OWClient/Server sends traffic in only one direction, to avoid cross
    traffic interference

---
+++ src/main/java/OWClient.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.net.InetAddress;
+
+/*
+        IP addr
+        train length (# of pairs)
+        # trains
+        size of Ph	(64 bytes default)
+        size of Pt 	(1500 bytes default)
+        resolution
+*/
+
+public class OWClient {
+
+    private static UDPClient client;
+    private static TCPClient control;
+
+    // arguments: String IP, int trainLength, int numTrains, int sH, int sT, int IPGmicro
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int header = 42;
+        String address = args[0];
+        int trainLength = Integer.parseInt(args[1]);
+        int numTrains = Integer.parseInt(args[2]);
+        int sH = Integer.parseInt(args[3])-header;
+        int sT = Integer.parseInt(args[4])-header;
+        int IPGmicro = Integer.parseInt(args[5]);
+        InetAddress IP = InetAddress.getByName(address);
+
+
+        client = new UDPClient();
+        control = new TCPClient(address,6789);
+
+        int[] results = new int[numTrains];
+
+        // find the roundtrip time gap to space future runs
+        int IPGnano = IPGmicro * 1000;
+        int IPGmilli = IPGnano / 1000000;
+        IPGnano = IPGnano % 1000000;
+
+        System.out.println("Inter-probe gap: " + ((IPGmilli*1000)+(IPGnano/1000)) + " microseconds.");
+
+        // primary experimental loop
+        for (int i = 0; i < numTrains; i++) {
+            // send control variables to server
+            control.send(control.socket, trainLength);
+            control.send(control.socket, sH);
+            control.send(control.socket, sT);
+
+            for (int j = 0; j < trainLength; j++) {
+                client.packetPairIPG(sH, sT, IP, 9876);
+                Thread.sleep(IPGmilli,IPGnano);
+            }
+
+            // reduce packet size by 100 bytes for each successive train
+            sH -= 100;
+            sT -= 100;
+        }
+
+        client.socket.close();
+    }
+
+    // given a resolution string, return the relevant divisor to get this unit from nanoseconds
+    // returns -1 if given invalid string
+    public static int getResolutionDivider(String resolution){
+        int div = -1;
+        switch (resolution){
+            case "":div = 1000000000;
+                break;
+            case "milli":div = 1000000;
+                break;
+            case "micro":div = 1000;
+                break;
+            case "nano":div = 1;
+        }
+        return div;
+    }
+}
---
+++ src/main/java/OWServer.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+
+  /*
+
+    parameters to receive
+        train length
+        # of trains
+        size of Ph
+        size of Pt
+
+     */
+
+public class OWServer {
+    public static void main(String[] args) throws IOException {
+        TCPServer control = new TCPServer(6789);
+        UDPServer server = new UDPServer(9876);
+        int header = 42;
+        PrintWriter writer = new PrintWriter("AvgIPG.txt", "UTF-8");
+        // main server loop, continually accepts new packet trains
+
+
+        while(true){
+            Socket connection = control.socket.accept();
+            int trainLength = control.receive(connection);
+            int sH = control.receive(connection);
+            int sT = control.receive(connection);
+            int div = 1000;
+
+            int sum = 0;
+            int numValid = 0;
+
+            for(int j = 0; j < trainLength; j++){
+                long IPG = server.packetPairIPG(sH,sT);
+                if(valid(IPG,sH,sT)){
+                    sum += IPG;
+                    numValid ++;
+                }
+            }
+
+            sum /= numValid;
+            sum /= div;
+            System.out.println("Head " + (sH+header) + " bytes\tTail " + (sT+header) + " bytes\tIntra-probe gap " + sum + " " + "micro" + "seconds");
+            writer.println(sum);
+        }
+
+    }
+
+    public static boolean valid(long IPG, int sH, int sT){
+        return true;
+    }
+}
