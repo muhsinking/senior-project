8de391105ed2a647f1db62f9ae881f60b05de538
muhsinking <muhsinking@gmail.com>
2016-03-30 14:14:46 -0400

    Add decrement argument and control variables

--- src/main/java/OWClient.java
+++ src/main/java/OWClient.java
@@ -15,7 +15,7 @@ public class OWClient {
     private static UDPClient client;
     private static TCPClient control;
 
-    // arguments: String IP, int trainLength, int numTrains, int sH, int sT, int IPGmicro
+    // arguments: String IP, int trainLength, int numTrains, int sH, int sT, int hDec, int tDec, int IPGmicro
     public static void main(String[] args) throws IOException, InterruptedException {
         int header = 42;
         String address = args[0];
@@ -23,41 +23,40 @@ public class OWClient {
         int numTrains = Integer.parseInt(args[2]);
         int sH = Integer.parseInt(args[3])-header;
         int sT = Integer.parseInt(args[4])-header;
-        int IPGmicro = Integer.parseInt(args[5]);
+        int hDec = Integer.parseInt(args[5]);
+        int tDec = Integer.parseInt(args[6]);
+        int IPGmicro = Integer.parseInt(args[7]);
         InetAddress IP = InetAddress.getByName(address);
-
 
         client = new UDPClient();
 
         int[] results = new int[numTrains];
 
         // find the roundtrip time gap to space future runs
-        // int IPGnano = IPGmicro * 1000;
-        int IPGmilli = IPGmicro / 1000;
-        //IPGnano = IPGnano % 1000000;
-	int IPGnano = 0;
+        int IPGnano = IPGmicro * 1000;
+        int IPGmilli = IPGnano / 1000000;
+        IPGnano = IPGnano % 1000000;
 
         System.out.println("Inter-probe gap: " + ((IPGmilli*1000)+(IPGnano/1000)) + " microseconds.");
+        control = new TCPClient(address,6789);
+        // send control variables to server
+        control.send(control.socket, numTrains);
+        control.send(control.socket, trainLength);
+        control.send(control.socket, sH);
+        control.send(control.socket, sT);
+        control.send(control.socket, hDec);
+        control.send(control.socket, tDec);
 
         // primary experimental loop
         for (int i = 0; i < numTrains; i++) {
-            control = new TCPClient(address,6789);
-            // send control variables to server
-            control.send(control.socket, trainLength);
-            control.send(control.socket, sH);
-            control.send(control.socket, sT);
-
             for (int j = 0; j < trainLength; j++) {
                 client.packetPairIPG(sH, sT, IP, 9876);
                 Thread.sleep(IPGmilli,IPGnano);
             }
-
-            // reduce packet size by 100 bytes for each successive train
-            sH -= 100;
-            sT -= 100;
-	    control.send(control.socket, 0);
+            // reduce packet size by the given decrements for each successive train
+            sH -= hDec;
+            sT -= tDec;
         }
-
         client.socket.close();
     }
 
--- src/main/java/OWServer.java
+++ src/main/java/OWServer.java
@@ -20,34 +20,40 @@ public class OWServer {
         TCPServer control = new TCPServer(6789);
         UDPServer server = new UDPServer(9876);
         int header = 42, div = 1000;
-        PrintWriter writer = new PrintWriter("AvgIPG.txt", "UTF-8");
-	
+//        PrintWriter writer = new PrintWriter("AvgIPG.txt", "UTF-8");
 
-        // main server loop, continually accepts new packet trains
+        // main server loop, continually accepts new packet train series
 
         while(true){
             // receive control variables from client
             Socket connection = control.socket.accept();
+            int numTrains = control.receive(connection);
             int trainLength = control.receive(connection);
             int sH = control.receive(connection);
             int sT = control.receive(connection);
+            int hDec = control.receive(connection);
+            int tDec = control.receive(connection);
 
-            int sum = 0;
-            int numValid = 0;
-	    System.out.print("\nHead " + (sH+header) + " bytes\tTail " + (sT+header) + " bytes\t");
-            for(int j = 0; j < trainLength; j++){
-                long IPG = server.packetPairIPG(sH,sT);
-                if(valid(IPG,sH,sT)){
-                    sum += IPG;
-                    numValid ++;
+            for(int i = 0; i < numTrains; i++){
+                int sum = 0;
+                int numValid = 0;
+                System.out.print("\nHead " + (sH+header) + " bytes\tTail " + (sT+header) + " bytes\t");
+
+                for(int j = 0; j < trainLength; j++){
+                    long IPG = server.packetPairIPG(sH,sT);
+                    if(valid(IPG,sH,sT)){
+                        sum += IPG;
+                        numValid ++;
+                    }
                 }
+
+                sum /= numValid;
+//                writer.println(sum);
+                System.out.print("Intra-probe gap " + sum/div + " " + "micro" + "seconds");
+                sH -= hDec;
+                sT -= tDec;
             }
 
-	    int whatever = control.receive(connection);
-
-            sum /= numValid;
-            System.out.print("Intra-probe gap " + sum/div + " " + "micro" + "seconds");
-            writer.println(sum);
         }
 
     }
